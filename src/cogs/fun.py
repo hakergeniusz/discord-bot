50722c889b89a3f8f991c7e13f7612a6ddde8628
hakergeniusz <piotrekwozniak2020@proton.me>
2026-01-23 22:19:46 +0100

    refactor(src/cogs/fun): remove unused library

--- src/cogs/fun.py
+++ src/cogs/fun.py
@@ -13,8 +13,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import asyncio
-
 import discord
 from discord import app_commands
 from discord.ext import commands
